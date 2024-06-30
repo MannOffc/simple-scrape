209a35a18a908f9aff07fb65c61e59f698f23158
MannHost <146666179+MannOffc@users.noreply.github.com>
2024-07-01 03:09:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-var leptonai = require('./ai/leptonai'), letmegpt = require('./ai/letmegpt'), thinkany = require('./ai/thinkany')
+//ai
+var aoyo = require('./ai/aoyo'), isouchat = require('./ai/isouchat'), leptonai = require('./ai/leptonai'), letmegpt = require('./ai/letmegpt'), thinkany = require('./ai/thinkany'), yousearch = require('./ai/yousearch')
+
+//downloader
+var tiktok = require('./downloader/tiktok'), twitter = require('./downloader/twitter')
 
 module.exports = {
 ai: {
+    aoyo: aoyo,
+    isouchat: isouchat,
     leptonai: leptonai,
     letmegpt: letmegpt,
-    thinkany: thinkany
+    thinkany: thinkany,
+    yousearch: yousearch
+},
+downloader: {
+    tiktok: tiktok,
+    twitter: twitter
 }
 }
