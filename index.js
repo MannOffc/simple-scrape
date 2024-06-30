b5d68e0b2c88f8d065e5d51888e813659cc14d96
MannHost <146666179+MannOffc@users.noreply.github.com>
2024-07-01 03:43:50 +0700

    MannR (⁠≧⁠▽⁠≦⁠)

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 //ai
-var aoyo = require('./ai/aoyo'), isouchat = require('./ai/isouchat'), leptonai = require('./ai/leptonai'), letmegpt = require('./ai/letmegpt'), thinkany = require('./ai/thinkany'), yousearch = require('./ai/yousearch')
+var aoyo = require('./ai/aoyo'), isouchat = require('./ai/isouchat') leptonai = require('./ai/leptonai'), letmegpt = require('./ai/letmegpt'), yousearch = require('./ai/yousearch')
 
 //downloader
 var tiktok = require('./downloader/tiktok'), twitter = require('./downloader/twitter')
+
+//internet
+var bukalapak = require('./internet/bukalapak'), kiryu = require('./internet/kiryu'), ttsearch = require('./internet/ttsearch')
+
+//tools
+var scanner = require('./tools/scanner')
 
 module.exports = {
 ai: {
     aoyo: aoyo,
     isouchat: isouchat,
     leptonai: leptonai,
-    letmegpt: letmegpt,
-    thinkany: thinkany,
-    yousearch: yousearch
+    letmegpt: letmegpt
 },
 downloader: {
     tiktok: tiktok,
-    twitter: twitter
+    twietter: twitter
+},
+internet: {
+    bukalapak: bukalapak,
+    kiryu: kiryu,
+    ttsearch: ttsearch
+},
+tools: {
+    scanner: scanner
 }
 }
